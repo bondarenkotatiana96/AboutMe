9333e72be038813ae40e1372419eb9bbcf4f197f
tanya_bond <bondarenkotatiana96@gmail.com>
2022-07-05 14:58:05 -0700

    Added about me content page

--- AboutMe/ContentView.swift
+++ AboutMe/ContentView.swift
@@ -9,9 +9,45 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, Tanya!")
-            .padding()
-            .foregroundColor(.red)
+        VStack(alignment: .leading) {
+            
+            ZStack(alignment: .leading) {
+                
+                Image("bannerImage")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                
+                Image("profilePicture")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 130, height: 130)
+                .clipShape(Circle())
+                .padding(.top, 200)
+//                .offset(y: 90)
+            }
+            .padding(.bottom)
+            
+            VStack(alignment: .leading, spacing: 4) {
+                HStack {
+                    Text("Tanya Bondarenko")
+                        .fontWeight(.bold)
+                        .font(.title)
+                    Image(systemName: "checkmark.seal.fill")
+                        .foregroundColor(.blue)
+                        .imageScale(.large)
+                }
+                
+                Text("iOS Developer")
+                    .font(.caption)
+                
+                Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.")
+                    .fontWeight(.light)
+                    .padding(.top)
+            }
+            .padding(.horizontal)
+            Spacer()
+        }
+        .background(.gray)
     }
 }
 
